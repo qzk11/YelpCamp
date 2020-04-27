c23bfe7a9b5fc8c19e02abb1103be00ad7eda1e8
qzk11 <616341668@qq.com>
2020-04-27 01:08:08 -0400

    using mongodb

--- app.js
+++ app.js
@@ -1,46 +1,84 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-var campgrounds = [
-        // {name: "name1", image:"https://media.istockphoto.com/photos/beautiful-woman-with-natural-makeup-picture-id897056188"},
-        // {name: "name1", image:"https://media.istockphoto.com/photos/beautiful-african-american-female-model-picture-id910856488"},
-        // {name: "name1", image:"https://media.istockphoto.com/photos/young-gorgeous-asian-woman-in-a-smoky-eyes-style-make-up-dressed-in-a-picture-id865326900"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-    ];
-app.get("/", function(req, res) {
+
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+// Campground.create(
+//      {
+//          name: "Granite Hill", 
+//          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//          description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
+         
+//      },
+//      function(err, campground){
+//       if(err){
+//           console.log(err);
+//       } else {
+//           console.log("NEWLY CREATED CAMPGROUND: ");
+//           console.log(campground);
+//       }
+//     });
+app.get("/", function (req, res) {
     res.render("landing");
 })
 
-app.get("/campgrounds", function(req,res) {
-    
-    res.render("campgrounds", {campgrounds: campgrounds});
+app.get("/campgrounds", function (req, res) {
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+           res.render("index",{campgrounds:allCampgrounds});
+        }
+     });
+    // res.render("campgrounds", { campgrounds: campgrounds });
 })
 
-app.post("/campgrounds", function(req,res) {
+app.post("/campgrounds", function (req, res) {
     // res.send("This is a post route");
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name:name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-
+    var desc = req.body.description;
+    var newCampground = { name: name, image: image, description: desc};
+    // campgrounds.push(newCampground);
+    // res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 })
 
-app.get("/campgrounds/new", function(req, res) {
+app.get("/campgrounds/new", function (req, res) {
     res.render("new");
 });
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+})
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("The YelpCamp Server has started!");
 });
