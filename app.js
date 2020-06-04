0fc318e479efc55234aa3638d865e9c260d17701
qzk11 <616341668@qq.com>
2020-06-04 15:04:16 -0400

    add mongolab

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ passport.deserializeUser(User.deserializeUser());
 
 // Database configuration
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://zhekunqi:zhekunqi@cluster0-arflj.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
